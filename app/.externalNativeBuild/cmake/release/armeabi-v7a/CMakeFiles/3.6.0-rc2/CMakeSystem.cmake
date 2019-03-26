41a72c14b2114347b9709eef137fb6067949d99c
THEMKR <manish.rewalliya@gmail.com>
2019-03-26 18:44:50 +0530

    Run on new system

--- app/.externalNativeBuild/cmake/release/armeabi-v7a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
+++ app/.externalNativeBuild/cmake/release/armeabi-v7a/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -1,9 +1,9 @@
-set(CMAKE_HOST_SYSTEM "Linux-4.15.0-32-generic")
+set(CMAKE_HOST_SYSTEM "Linux-4.4.0-143-generic")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "4.15.0-32-generic")
+set(CMAKE_HOST_SYSTEM_VERSION "4.4.0-143-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("/home/delhivery/Android/Sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
+include("/home/administrator/Android/Sdk/ndk-bundle/build/cmake/android.toolchain.cmake")
 
 set(CMAKE_SYSTEM "Android-1")
 set(CMAKE_SYSTEM_NAME "Android")
