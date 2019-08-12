2a6cc1e111d864e5358b1f3e64ef171d88e59c8d
THEMKR <manish.rewalliya@gmail.com>
2019-08-12 16:05:46 +0530

    Port to android X

--- app/src/main/java/com/mkrworld/libfilter/MainActivity.kt
+++ app/src/main/java/com/mkrworld/libfilter/MainActivity.kt
@@ -2,8 +2,8 @@ package com.mkrworld.libfilter
 
 import android.graphics.*
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
